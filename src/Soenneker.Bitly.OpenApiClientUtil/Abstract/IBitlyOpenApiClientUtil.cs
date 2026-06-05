6033a7217fc933d8def722f434e4398eb4f73b31
Jake Soenneker <4441470+soenneker@users.noreply.github.com>
2026-06-04 19:37:58 -0500

    xml commenting

--- src/Soenneker.Bitly.OpenApiClientUtil/Abstract/IBitlyOpenApiClientUtil.cs
+++ src/Soenneker.Bitly.OpenApiClientUtil/Abstract/IBitlyOpenApiClientUtil.cs
@@ -10,5 +10,10 @@ namespace Soenneker.Bitly.OpenApiClientUtil.Abstract;
 /// </summary>
 public interface IBitlyOpenApiClientUtil: IDisposable, IAsyncDisposable
 {
+    /// <summary>
+    /// Gets the value.
+    /// </summary>
+    /// <param name="cancellationToken">The cancellation token.</param>
+    /// <returns>A task containing the result of the operation.</returns>
     ValueTask<BitlyOpenApiClient> Get(CancellationToken cancellationToken = default);
 }
